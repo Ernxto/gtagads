afe3194d311d07d6cbd8d3acb0f439330f39e46e
vigikaran vijayaratnam <vigikaran.php@gmail.com>
2020-04-30 16:27:56 +0100

    bug fixed

--- gtagads.js
+++ gtagads.js
@@ -1,9 +1,9 @@
 (function () {
+    const a = document.getElementsByClassName('top-eds')[0];
     setTimeout(function () {
         console.log(`gtag`)
-        const a = document.getElementsByClassName('top-eds')[0]
-        a.innerHTML = `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>`
-        (adsbygoogle = window.adsbygoogle || []).push({})
+        a.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+        (adsbygoogle = window.adsbygoogle || []).push({});
     }, 5000)
 }())
 
