1c3b862d250631cc64e9e1e83b8073cdf759dc41
vigikaran vijayaratnam <vigikaran.php@gmail.com>
2020-04-30 16:38:20 +0100

    timer reduced

--- gtagads.js
+++ gtagads.js
@@ -5,6 +5,6 @@
         console.log(`gtag run`)
         a.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>';
         (adsbygoogle = window.adsbygoogle || []).push({});
-    }, 5000)
+    }, 2000)
 }())
 
