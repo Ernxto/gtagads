ee3d0603b0ed2b9ae6d7178967b20f307df6ff0e
vigikaran vijayaratnam <vigikaran.php@gmail.com>
2020-04-30 17:07:10 +0100

    desktop only added

--- gtagads.js
+++ gtagads.js
@@ -1,10 +1,12 @@
 (function () {
     console.log(`gtag init`)
-    const a = document.getElementsByClassName('top-eds')[0];
-    setTimeout(function () {
-        console.log(`gtag run`)
-        a.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>';
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }, 2000)
+    const a = document.getElementsByClassName('top-eds');
+    if(a.length > 0 ){
+        setTimeout(function () {
+            console.log(`gtag run`)
+            a[0].innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        }, 2000)
+    }
 }())
 
