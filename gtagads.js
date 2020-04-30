2b1e97e6ec1f94e49565946e82e2ebdd49263911
vigikaran vijayaratnam <vigikaran.php@gmail.com>
2020-04-30 14:22:34 +0100

    ads enabled

--- gtagads.js
+++ gtagads.js
@@ -1,4 +1,6 @@
 (function () {
-    console.log(`gtag initialized`)    
+    const a = document.getElementsByClassName('top-eds')[0]
+    a.innerHTML = `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>`
+    (adsbygoogle = window.adsbygoogle || []).push({})
 }())
 
