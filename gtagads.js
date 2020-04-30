9b10385f190582ac7a0f777af163d12a1ca30063
vigikaran vijayaratnam <vigikaran.php@gmail.com>
2020-04-30 16:36:13 +0100

    bug fixed

--- gtagads.js
+++ gtagads.js
@@ -1,7 +1,8 @@
 (function () {
+    console.log(`gtag init`)
     const a = document.getElementsByClassName('top-eds')[0];
     setTimeout(function () {
-        console.log(`gtag`)
+        console.log(`gtag run`)
         a.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>';
         (adsbygoogle = window.adsbygoogle || []).push({});
     }, 5000)
