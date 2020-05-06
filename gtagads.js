0394e597ed179360d8932ab50f526944339dd569
vigikaran vijayaratnam <vigikaran.php@gmail.com>
2020-05-06 13:07:46 +0100

    Inventory conditions added

--- gtagads.js
+++ gtagads.js
@@ -1,12 +1,35 @@
 (function () {
     console.log(`gtag init`)
-    const a = document.getElementsByClassName('top-eds');
-    if(a.length > 0 ){
-        setTimeout(function () {
-            console.log(`gtag run`)
-            a[0].innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3421167218565283" data-ad-slot="2399260720" data-ad-format="auto" data-full-width-responsive="true"></ins>';
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        }, 2000)
-    }
+    const now = new Date()
+    const c = window.userGeo || 'gb'
+    const client = 'ca-pub-3421167218565283'
+    const inv = [
+        {
+            id : 'dfp_banner_header_panel',
+            inv: 2399260720,
+            geo : ['gb','ca'],
+            time : {
+                from : 10,
+                to: 15
+            }
+        }
+    ];
+    setTimeout(function () {
+        console.log(`gtag run`)
+        inv.forEach(i => {
+            try {
+                if(i.geo.length === 0 || i.geo.includes(c.toLowerCase())){
+                    if(i.time === null || (now.getHours() >= i.time.from && now.getHours() <= i.time.to )){
+                        const a = document.getElementById(i.id)
+                        if(a && a !== null){
+                            a.innerHTML = `<ins class="adsbygoogle" style="display:block" data-ad-client="${client}" data-ad-slot="${i.inv}" data-ad-format="auto" data-full-width-responsive="true"></ins>`;
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        }
+                    }
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }, 2000)
 }())
-
